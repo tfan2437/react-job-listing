14c5f90e6cff381eedcd723319875e4af26d2af4
tfan2437 <tfan2437@gmail.com>
2024-05-30 01:41:57 -0400

    Add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Testbar from "./components/Testbar";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       {/* <Testbar /> */}
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <h1>Become a Web Developer</h1>
+      <p>Find the job that fits your skills and needs</p>
+    </div>
+  );
+};
+
+export default Hero;
